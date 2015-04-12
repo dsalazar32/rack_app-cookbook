aea2bfc39414a2d59408889c6093f635cf5d52b0
David Salazar <dsalazar@iomediums.com>
2015-04-11 21:03:30 -0400

    Bumped metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dsalazar@iomediums.com'
 license          'All rights reserved'
 description      'Installs/Configures rack_app'
 long_description 'Installs/Configures rack_app'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'packagecloud', '~> 0.0.17'
 depends 'nginx', '~> 2.7.6'
